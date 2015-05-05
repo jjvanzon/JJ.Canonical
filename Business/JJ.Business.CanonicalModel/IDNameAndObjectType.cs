058342234d5145961c5442ab1960fabdfd8d68f2
jjvanzon <janjoostvanzon@hotmail.com>
2015-05-05 18:25:55 +0000

    Synthesizer: deleting documents with related entities

--- Business/JJ.Business.CanonicalModel/IDNameAndObjectType.cs
+++ Business/JJ.Business.CanonicalModel/IDNameAndObjectType.cs
@@ -6,11 +6,12 @@ using System.Text;
 namespace JJ.Business.CanonicalModel
 {
     /// <summary>
-    /// TODO: Rename to IDAndName.
+    /// TODO: Remove class if no longer used.
     /// </summary>
-    public class IDName
+    public class IDNameAndObjectType
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public string ObjectType { get; set; }
     }
 }
