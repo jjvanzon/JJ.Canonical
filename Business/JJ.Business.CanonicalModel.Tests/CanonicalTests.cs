6302d305c2dc4d6cb7467e864b792b31eb23fac7
jjvanzon <jjvanzon79@gmail.com>
2025-03-28 03:09:58 +0100

    🧪 JJ.Canonical.Tests: Add minimal test for ValidationMessage to document current usage and its quirks.

---
+++ Business/JJ.Business.CanonicalModel.Tests/CanonicalTests.cs
@@ -0,0 +1,27 @@
+﻿// ReSharper disable UnusedVariable
+namespace JJ.Business.CanonicalModel.Tests;
+
+[TestClass]
+public sealed class CanonicalTests
+{
+    [TestMethod]
+    public void Test_Canonical_IDAndName()
+    {
+        var model = new IDAndName<string>
+        {
+            ID   = 1, // Flaw: Type argument <string> is actually not used. ID is still integer here.
+            Name = "My Name"
+        };
+    }
+
+    [TestMethod]
+    public void Test_Canonical_ValidationMessage()
+    {
+        // In hindsight, just a string would have sufficed — newer versions will simplify this.
+        var model = new ValidationMessage
+        {
+            PropertyKey = "MyProp",
+            Text = "Hello"
+        };
+    }
+}
