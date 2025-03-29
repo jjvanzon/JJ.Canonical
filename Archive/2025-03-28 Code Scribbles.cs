cbb9064281fe66a3fb19c39185a167ef625158da
jjvanzon <jjvanzon79@gmail.com>
2025-03-29 19:36:46 +0100

    🗄️ Archive idea scribble

--- Archive/2025-03-28 Code Scribbles.cs
+++ Archive/2025-03-28 Code Scribbles.cs
@@ -9,3 +9,17 @@
     <!--<TargetFrameworks>net9.0;net8.0;net7.0;net6.0;net5.0;net48;net481;net461</TargetFrameworks>-->
 
 JJ.Canonical contains shared data types used across JJ projects.
+
+-----
+
+Idea: ContentFiles for default .props
+
+I used this in nuspec in another project:
+
+```
+    <contentFiles>
+      <files include="Scripts/*.js" buildAction="Content" />
+    </contentFiles>
+```
+
+But don't know the syntax in a csproj file.
